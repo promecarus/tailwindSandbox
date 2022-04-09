089e0eb794217de502f72645f22043ad988ec249
Muhammad Haikal Al Rasyid <haikalslipi@gmail.com>
2022-04-09 22:05:53 +0700

    save point

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
 	content: ["index.html"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#262322",
+				secondary: "#efefef",
+				accent: "#7f7caf",
+			},
+		},
 	},
 	plugins: [],
 };
